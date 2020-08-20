98062383b1bbadb84c7a242aace341cc80d53119
Angel Acosta <Mrnurdee@gmail.com>
2020-08-20 08:14:58 -0700

    Add project files.

---
+++ KiloToMiles/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace KiloToMiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter kilometers:");
+            double kilometers = Convert.ToDouble(Console.ReadLine());
+            double miles = kilometers / 1.609;
+            Console.WriteLine(miles + " Miles");
+        }
+    }
+}
